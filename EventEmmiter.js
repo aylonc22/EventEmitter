1c48a6a053a8fd32ead5bc2b20b45f39944b3deb
Aylon <aylonc1@gmail.com>
2024-10-23 22:20:36 +0300

    added eventEmmiter implementetion

---
+++ EventEmmiter.js
@@ -0,0 +1,37 @@
+class EventEmitter{
+    constructor(){
+        this.events = {};
+    }
+
+    //Subscribe to an event
+    on(event,listener){
+        if(!this.events[event]){
+            this.events[event] = [];
+        }
+        this.events[event].push(listener);
+    }
+
+    //Emit an event
+    emit(event,...args){
+        if(this.events[event]){
+            this.events[event].forEach(listener => {
+                listener(...args)
+            });
+        }
+    }
+
+    //Unsubscripe from an event
+    off(event,listener){
+        if(!this.events[event])return;
+        this.events[event] = this.events[event].filter(l=>l!==listener);
+    }
+
+    //Subscrive to an event only once
+    once(event,listener){
+        const onceWrapper = (...args)=>{
+            listener(...args);
+            this.off(event,onceWrapper);
+        };
+        this.on(event,onceWrapper);
+    }
+}
